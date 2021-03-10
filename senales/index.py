09bbb591abb64d255e24b05c9d6b283e32ce2b55
luisescobar09 <1719110043@utectulancingo.edu.mx>
2021-03-10 22:07:51 +0000

    Web App de señales de transito funcionando

--- senales/index.py
+++ senales/index.py
@@ -12,9 +12,14 @@ render = web.template.render("senales/")
 fileForm = form.Form(form.File('myfile'))
 
 class Index(object):
+
+
+
     def GET(self):
         f = fileForm()
         return render.index(f)
+        #self.datos = None
+        #return render.index(self.datos)
 
     def POST(self):
         x = web.input(myfile={})
@@ -57,19 +62,32 @@ class Index(object):
             # run the inference
             prediction = model.predict(data)
             print(prediction)
-            for i in prediction:
+            cabeza = "<html lang='es'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'><title>Señales de transito</title><!-- CSS only -->	<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1' crossorigin='anonymous'><link rel='icon' href='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Mexico_traffic_signal_sr7.svg/1200px-Mexico_traffic_signal_sr7.svg.png' type='image/png' /></head><body>    <div class='container bg-light '>		<div class='row justify-content-center mt-4 pt-4'>			<div class='col-md-8 '>"
+            final = "</div>        </div>    </div></body></html>"
+            for i in prediction: 
                 if i[0] > 0.65:
                     print("Es una señal de zona de derrumbes.")
+                    titulo= cabeza+"<h1 class='text-danger text-center'>La imagen es una señal de zona de derrumbes.</h1> <br> <br> <h2 class='text-info'>Descripción:</h2> <h3>Advierte sobre una zona en la cual pueden ocurrir derrumbes.</h3><br> <img src='https://lh3.googleusercontent.com/proxy/cAtcnptQkqZJhBNnOhXXYe-pgj8pQ-ZcdICFlng4ga60V8hwEhZQ81Nzwu1EclDSCgyxhtBhTAKdlzas9RvOXJ7hMrB_y7fy0vwM9drzZ6XGFyCK0X93xJICSKpV5xgBz8xMxaNNQvsVaISQIDhifBPd9D7c-gPaAz5Zjj89wjUZKzYe' align='center' width='300' height='275'> <br><br>"+final
                 elif i[1] > 0.65:
                     print("Es una señal de doble sentido.")
+                    titulo= cabeza+"<h1 class='text-danger text-center'>La imagen es una señal de doble circulación.</h1> <br> <br> <h2 class='text-info'>Descripción:</h2> <h3>El señalamiento se utiliza para marcar el camino de circulación en un solo sentido o en doble sentido.</h3><br> <img src='https://lh3.googleusercontent.com/proxy/zkG_8nlSfo3smpmkAlDlFBPpoS-bfjSsQcxb8Wrmrj0S7b1TOTbmCLg5tlo9fjJnMBsRXdiI8jHLEhgUQwdqpfRaoUFn37KCnOYzXZNeVEF6IfmlrbkvirwkbqqnADbLgjRfFVzsxbELU5HjorW4PbIQQX49mjrwS1jJQ6c' align='center' width='300' height='275'> <br><br>"+final
+
                 elif i[2] > 0.65:
                     print("Es una señal de interseccion.")
+                    titulo = "La imagen es una señal de interseccion."
+                    titulo= cabeza+"<h1 class='text-danger text-center'>La imagen es una señal de interscción (entronque).</h1> <br> <br> <h2 class='text-info'>Descripción:</h2> <h3>La línea mas ancha señalara el camino principal, mientras que la mas angosta el camino secundario.</h3><br> <img src='https://www.puedomanejar.com/wp-content/uploads/2020/12/4wayintersection.png' align='center' width='300' height='275'> <br><br>"+final
                 elif i[3] > 0.65:
                     print("Es una señal de salida por lateral")
+                    titulo= cabeza+"<h1 class='text-danger text-center'>La imagen es una señal de Incorporación de transito.</h1> <br> <br> <h2 class='text-info'>Descripción:</h2> <h3>Este tipo de señalamiento avisa sobre la incorporación de transito que va en la misma.</h3><br> <img src='https://lh3.googleusercontent.com/proxy/4Bhtmwyey0ukSpmwrHYgcyGgZYZ3gPO4jY6YVRmkkaSuUhC5JvgxNZdQaADAblb9peuYtGql2NHt__fFfGYMBHk-7hqkfpffnVOQzt0LSavylxDuZSmsXWWP_k1eC9OWIK6vAmIPGQsllcbmAHuBNPBPEXIsoVdYe6lhGdIjhKZjtb9Z9-k9oWs2MbKO' align='center' width='300' height='275'> <br><br>"+final
                 elif i[4] > 0.65:
                     print("Es una señal de peatón.")
+                    titulo= cabeza+"<h1 class='text-danger text-center'>La imagen es una señal de peatón.</h1> <br> <br> <h2 class='text-info'>Descripción:</h2> <h3>Indica un camino con constante paso peatonal o cruce peatonal en específico.</h3><br> <img src='https://lh3.googleusercontent.com/proxy/PuaYJPyit2CUmkOHFSTlLoNcdTIsQYStXSqMvDabwUzoHoV5M79x3zMkpkrWOJav6Z54IHqQRzNyKXL7-LL73QLqVrczp5auzIzm0Cx5E_R-F29oGMND__wZQxED4OLkqL3L2IhMb2h6lD97rQ' align='center' width='300' height='275'> <br><br>"+final
                 elif i[5] > 0.65:
                     print("Es una señal de tope.")
+                    titulo= cabeza+"<h1 class='text-danger text-center'>La imagen es una señal de tope.</h1> <br> <br> <h2 class='text-info'>Descripción:</h2> <h3>Advierte la proximidad de una protuberancia en la superficie de la vía.</h3><br> <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Mexico_road_sign_SP-41.svg/1024px-Mexico_road_sign_SP-41.svg.png' align='center' width='300' height='275'> <br><br>"+final
                 else:
+                    
                     print("No pudimos interpretar la imagen, intenta de nuevo")
-        
+                    titulo= cabeza+"<h1 class='text-danger text-center'>No pudimos interpretar la imagen, intenta de nuevo.</h1> <br> <br>  <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a8eeb0c9-ea86-40dd-8a7a-91912aab7fc2/de2npbi-77dd5cf3-83d9-41bf-9839-8b595c550c9d.png/v1/fill/w_1035,h_772,strp/quico_marciano_by_xocotzin_r_de2npbi-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD05NTUiLCJwYXRoIjoiXC9mXC9hOGVlYjBjOS1lYTg2LTQwZGQtOGE3YS05MTkxMmFhYjdmYzJcL2RlMm5wYmktNzdkZDVjZjMtODNkOS00MWJmLTk4MzktOGI1OTVjNTUwYzlkLnBuZyIsIndpZHRoIjoiPD0xMjgwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.0BeLYI-HZ4ZdrYUerJWoFeZGjKeg5VzFuLjPVPkaXCw' align='center' width='850' height='475'> <br><br>"+final
+                    
+        return titulo
