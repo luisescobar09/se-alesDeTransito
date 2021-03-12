d355f12c9532d26a9f9854fee949d9b5f99a9d77
luisescobar09 <1719110043@utectulancingo.edu.mx>
2021-03-12 02:28:21 +0000

    API REST de clasificación de imagenes funcionando

--- app2.py
+++ app2.py
@@ -1,63 +1,111 @@
+import web, shutil
+from web import form
+import os
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
+import json
 
-# Disable scientific notation for clarity
-np.set_printoptions(suppress=True)
+urls = ('/index', 'Index')
 
-# Load the model
-model = tensorflow.keras.models.load_model('keras_model.h5')
+class Index(object):
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    def POST(self):
+        x = web.input(myfile={})
+        filedir = '/workspace/se-alesDeTransito/senales/static/'
+        if 'myfile' in x:
+            filepath=x.myfile.filename.replace('\\','/')
+            filename=filepath.split('/')[-1]
+            fout = open(filedir +'/'+ filename,'wb')
+            fout.write(x.myfile.file.read())
+            fout.close()
+            np.set_printoptions(suppress=True)
 
-# Replace this with the path to your image
-image = Image.open('2.png')
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
+            # Load the model
+            model = tensorflow.keras.models.load_model('/workspace/se-alesDeTransito/senales/static/keras_model.h5')
 
-#turn the image into a numpy array
-image_array = np.asarray(image)
+            # Create the array of the right shape to feed into the keras model
+            # The 'length' or number of images you can put into the array is
+            # determined by the first position in the shape tuple, in this case 1.
+            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-# display the resized image
-image.show()
+            # Replace this with the path to your image
+            image = Image.open('/workspace/se-alesDeTransito/senales/static/'+filename)
+            #resize the image to a 224x224 with the same strategy as in TM2:
+            #resizing the image to be at least 224x224 and then cropping from the center
+            size = (224, 224)
+            image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+            #turn the image into a numpy array
+            image_array = np.asarray(image)
 
-# Load the image into the array
-data[0] = normalized_image_array
+            # display the resized image
+            image.show()
 
-# run the inference
-prediction = model.predict(data)
-print(prediction)
-for i in prediction:
-    if i[0] > 0.65:
-        print("Es una señal de curva hacia la derecha.")
-    elif i[1] > 0.65:
-        print("Es una señal de zona de derrumbes.")
-    elif i[2] > 0.65:
-        print("Es una señal de doble sentido.")
-    elif i[3] > 0.65:
-        print("Es una señal de estrechamiento.")
-    elif i[4] > 0.65:
-        print("Es una señal de interseccion.")
-    elif i[5] > 0.65:
-        print("Es una señal de lateral.")
-    elif i[6] > 0.65:
-        print("Es una señal de cruce de peatones.")
-    elif i[7] > 0.65:
-        print("Es una señal de rotonda.")
-    elif i[8] > 0.65:
-        print("Es una señal de camino sinuoso.")
-    elif i[9] > 0.65:
-        print("Es una señal de tope.")
-    elif i[10] > 0.65:
-        print("Es una señal de tunel.")
-    else:
-        print("No pudimos interpretar la imagen, intenta de nuevo")
+            # Normalize the image
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
+            # Load the image into the array
+            data[0] = normalized_image_array
+
+            # run the inference
+            prediction = model.predict(data)
+
+            for i in prediction: 
+                if i[0] > 0.65:
+                    titulo = "derrumbes"
+                    resultado = "La imagen es una señal de zona de derrumbes."
+                    descripcion = "Advierte sobre una zona en la cual pueden ocurrir derrumbes."
+                    status = 200
+
+                elif i[1] > 0.65:
+                    titulo = "doble"
+                    resultado = "La imagen es una señal de doble circulación."
+                    descripcion = "El señalamiento se utiliza para marcar el camino de circulación en un solo sentido o en doble sentido."
+                    status = 200
+
+                elif i[2] > 0.65:
+                    titulo = "intersección"
+                    resultado = "La imagen es una señal de intersección (entronque)."
+                    descripcion = "La línea mas ancha señalara el camino principal, mientras que la mas angosta el camino secundario."
+                    status = 200
+
+                elif i[3] > 0.65:
+                    titulo = "lateral"
+                    resultado = "La imagen es una señal de incorporación de transito."
+                    descripcion = "Este tipo de señalamiento avisa sobre la incorporación de transito que va en la misma."
+                    status = 200
+
+                elif i[4] > 0.65:
+                    titulo = "peatón"
+                    resultado = "La imagen es una señal de peatón."
+                    descripcion = "Indica un camino con constante paso peatonal o cruce peatonal en específico."
+                    status = 200
+
+                elif i[5] > 0.65:
+                    titulo = "tope"
+                    resultado = "La imagen es una señal de tope."
+                    descripcion = "Advierte la proximidad de una protuberancia en la superficie de la vía"
+                    status = 200
+
+                else:  
+                    titulo = "error"
+                    resultado = "No pudimos interpretar la imagen, intenta de nuevo."
+                    descripcion = "Error 404, not found"
+                    status = 404
+        
+        datos = {
+            titulo: [
+            ]
+        }
+
+        senal = {}
+        senal["resultado"] = resultado
+        senal["descripcion"] = descripcion
+        senal["status"] = status
+        datos[titulo].append(senal)
+        return json.dumps(datos)
+
+if __name__ == "__main__":
+   app = web.application(urls, globals()) 
+   app.run()
